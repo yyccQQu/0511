805fa6f5b45c664c1c5084942acc70c2f14e20ff
yyccQQu <643929860@qq.com>
2018-05-20 17:01:51 +0800

    用户列表完成

--- test/src/app.jsx
+++ test/src/app.jsx
@@ -14,6 +14,7 @@ import Layout from 'component/layout/index.jsx';
 import Home from 'page/home/index.jsx';
 import Login from 'page/login/index.jsx';
 import ErrorPage from 'page/error/index.jsx';
+import UserList from 'page/user/index.jsx';
 
 export default class App extends Component {
   render() {
@@ -23,6 +24,8 @@ export default class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/product" component={Home} />
             <Route path="/product-category" component={Home} />
+            <Route path="/user/index" component={UserList}/>
+            <Redirect exact from="/user" to="/user/index" />
              {/*<Route path="/order/index" component={OrderList} />
             <Route path="/order/detail/:orderNumber" component={OrderDetail} />
             <Route path="/user/index" component={UserList} />
