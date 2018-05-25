dd7a5c6e970a8b49ccf50c2e94e39f7bbb21c490
yyccQQu <643929860@qq.com>
2018-05-25 21:43:50 +0800

    forbase jsx

--- test/src/app.jsx
+++ test/src/app.jsx
@@ -12,6 +12,7 @@ import "font-awesome/css/font-awesome.min.css";
 
 import Layout from 'component/layout/index.jsx';
 import Home from 'page/home/index.jsx';
+import ProductRouter from 'page/product/router.jsx';
 import Login from 'page/login/index.jsx';
 import ErrorPage from 'page/error/index.jsx';
 import UserList from 'page/user/index.jsx';
@@ -22,8 +23,8 @@ export default class App extends Component {
       <Layout>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/product" component={Home} />
-            <Route path="/product-category" component={Home} />
+            <Route path="/product" component={ProductRouter} />
+            <Route path="/product-category" component={ProductRouter} />
             <Route path="/user/index" component={UserList}/>
             <Redirect exact from="/user" to="/user/index" />
              {/*<Route path="/order/index" component={OrderList} />
